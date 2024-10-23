c750d54ef611eee77454e45a5c0b7e8591a810ff
huanmeng-qwq <1871735932@qq.com>
2024-10-24 01:03:06 +0800

    feat: 添加带有ClickType类型参数的接口

---
+++ core/src/main/java/me/huanmeng/opensource/bukkit/gui/button/function/PlayerSimpleClickUpdateAllInterface.java
@@ -0,0 +1,33 @@
+package me.huanmeng.opensource.bukkit.gui.button.function;
+
+import me.huanmeng.opensource.bukkit.gui.AbstractGui;
+import me.huanmeng.opensource.bukkit.gui.enums.Result;
+import me.huanmeng.opensource.bukkit.gui.slot.Slot;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryType;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * 2024/10/23<br>
+ * Gui<br>
+ *
+ * @author huanmeng_qwq
+ */
+@FunctionalInterface
+public interface PlayerSimpleClickUpdateAllInterface extends PlayerClickInterface {
+    @Override
+    @NonNull
+    default Result onClick(@NonNull AbstractGui<?> gui, @NonNull Slot slot, @NonNull Player player, @NonNull ClickType click, @NonNull InventoryAction action, InventoryType.@NonNull SlotType slotType, int slotKey, int hotBarKey) {
+        onPlayerClick(player, click);
+        return Result.CANCEL_UPDATE_ALL;
+    }
+
+    /**
+     * 点击事件, 将会自动返回{@link Result#CANCEL_UPDATE_ALL}
+     *
+     * @param player 玩家
+     */
+    void onPlayerClick(@NonNull Player player, @NonNull ClickType click);
+}
