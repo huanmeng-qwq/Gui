807d24cbb50d46d6395c732d8baa450aadfcf81a
huanmeng-qwq <1871735932@qq.com>
2024-12-28 00:15:35 +0800

    feat: remove bstats

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,7 +6,6 @@ dependencies {
     implementation("net.kyori:adventure-api:4.14.0")
     implementation("net.kyori:adventure-platform-bukkit:4.3.1")
     implementation("net.kyori:adventure-text-serializer-legacy:4.14.0")
-    implementation("org.bstats:bstats-bukkit:3.0.2")
     compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
 }
 
