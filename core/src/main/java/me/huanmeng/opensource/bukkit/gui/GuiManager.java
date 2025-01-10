7b1e18aa43a2f27daff0f846c6effd4293fd2f08
huanmeng-qwq <1871735932@qq.com>
2025-01-10 23:06:28 +0800

    build: Bump version to 2.4.6

--- core/src/main/java/me/huanmeng/opensource/bukkit/gui/GuiManager.java
+++ core/src/main/java/me/huanmeng/opensource/bukkit/gui/GuiManager.java
@@ -81,7 +81,7 @@ public class GuiManager implements ListenerAdapter {
         this.plugin = plugin;
         this.audiences = BukkitAudiences.create(plugin);
         if (!Boolean.getBoolean("gui.disable-bStats")) {
-            metrics = new Metrics(plugin, 18670, "2.4.5");
+            metrics = new Metrics(plugin, 18670, "2.4.6");
         }
         if (registerListener) {
             try {
