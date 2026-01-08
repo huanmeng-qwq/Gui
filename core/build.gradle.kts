16ecdc9c7eab0edabdc55d80859fba90b515b577
huanmeng-qwq <1871735932@qq.com>
2026-01-08 22:07:35 +0800

    build: Change adventure dependencies to compileOnly
    - Change adventure-api, adventure-text-serializer-legacy from implementation to compileOnly
    - Add adventure-platform-api and adventure-text-serializer-gson as compileOnly dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,9 +3,10 @@ plugins {
 }
 
 dependencies {
-    implementation(libs.adventure.api)
-    implementation(libs.adventure.bukkit)
-    implementation(libs.adventure.text.serializer.legacy)
+    compileOnly(libs.adventure.api)
+    compileOnly(libs.adventure.text.serializer.legacy)
+    compileOnly(libs.adventure.text.serializer.gson)
+    compileOnly(libs.adventure.platform.api)
     compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
 }
 
